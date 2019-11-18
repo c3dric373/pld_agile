bfcfbb31694ca6e23b098216e712666e68032212
cmc373 <milinaire.c@gmail.com>
2019-11-18 16:10:45 +0100

    delivery proces + tour

--- code/src/main/java/model/data/DeliveryProcess.java
+++ code/src/main/java/model/data/DeliveryProcess.java
@@ -1,4 +1,28 @@
 package model.data;
 
+/**
+ * This class represents one Deliveryprocess. For this it has two ActionPoints one Pick Up point and one Delivery Point
+ */
 public class DeliveryProcess {
+
+    /**
+     * pickUp point
+     */
+    ActionPoint pickUP;
+
+    /**
+     * Delivery point
+     */
+    ActionPoint delivery;
+
+    /**
+     * Instatiates a Delivery process
+     * @param pickUP pickUp point of the delivery
+     * @param delivery delivery point of the deliver
+     */
+    DeliveryProcess(ActionPoint pickUP, ActionPoint delivery){
+        this.pickUP=pickUP;
+        this.delivery=delivery;
+    }
+
 }
--- code/src/main/java/model/data/Tour.java
+++ code/src/main/java/model/data/Tour.java
@@ -1,4 +1,51 @@
 package model.data;
 
+import org.apache.commons.lang.Validate;
+
+import java.util.List;
+
+/** A tour consists of a list of DeliveryProcesses, a base point and a start time. The list of deliveryProcesses are all
+ * the deliverys that the cyclists should complete
+ */
 public class Tour {
+
+    /**
+     * List of all the deliveries the cyclist has to do
+     */
+    private List<DeliveryProcess> deliveryProcesses;
+
+    /**
+     *Start point of the delivery
+     */
+    private Point base;
+
+    /**
+     * Start time of the delivery
+     */
+    private int startTime;
+
+    /**
+     * Instatiates a Tour
+     * @param deliveryProcesses list of deliveries
+     * @param base start point
+     * @param startTime start tim
+     */
+    Tour(List<DeliveryProcess> deliveryProcesses, Point base, int startTime){
+        Validate.notNull(deliveryProcesses, "deliveryProcess is null");
+        Validate.notNull(base,"base is null");
+        if (startTime<0){
+            throw new IllegalArgumentException("startTime is negative");
+        }
+        if (startTime>2359){
+            throw new IllegalArgumentException("startTime is too great");
+        }
+        this.deliveryProcesses=deliveryProcesses;
+        this.base=base;
+        this.startTime = startTime;
+    }
+
+
+
+
+
 }
