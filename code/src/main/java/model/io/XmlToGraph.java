c12d167588520a8d517348c75a16a5b154b2554e
corentin laharotte <colapala@gmail.com>
2019-11-25 09:42:38 +0100

    review comments

--- code/src/main/java/model/io/XmlToGraph.java
+++ code/src/main/java/model/io/XmlToGraph.java
@@ -17,7 +17,7 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class XmlToGraph {
-    /*
+    /**
      * ArrayList that contains nodes that we'll send at the end of the reading
      * Represents the graph
      */
@@ -25,44 +25,35 @@ public class XmlToGraph {
 
     public static void main(final String[] args) {
         ArrayList<Point> noeud = getGraphFromXml("C:\\Users\\colap\\Documents\\4IF\\pld_agile\\fichiersXML2019\\petitPlan.xml");
-        for (Point n: noeud ){
-            System.out.println(n.getId());
-            ArrayList<Segment> g= n.getNeighbourSegments();
-            System.out.println("----------");
-            for ( Segment f :g){
-                System.out.println(f.getName());
-            }
-            System.out.println("----------");
-        }
     }
 
     public static ArrayList<Point> getGraphFromXml(String url){
         nodes=new ArrayList<Point>();
-        /*
+        /**
          * Get an instance of class "DocumentBuilderFactory"
          */
         final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         try {
-            /*
+            /**
              * Creation of a parser
              */
             final DocumentBuilder builder = factory.newDocumentBuilder();
-            /*
+            /**
              * Creation of a document
              */
             final Document document = builder.parse(new File(url));
-            /*
+            /**
              * Get the root Element
              */
             final Element root = document.getDocumentElement();
-            /*
+            /**
              * Get the nodes tag and display the number of nodes
              */
             final NodeList nodeList = root.getElementsByTagName("noeud");
             final int nbNodeElements = nodeList.getLength();
             System.out.println(nbNodeElements);
 
-            /*
+            /**
              * Reading of all nodes in the file and addition to the ArrayList
              */
             for (int nodeIndex = 0; nodeIndex < nbNodeElements; nodeIndex++) {
@@ -74,14 +65,14 @@ public class XmlToGraph {
                 nodes.add(point);
             }
 
-            /*
+            /**
              * Get the segments tag and display the number of segments
              */
             final NodeList roadList = root.getElementsByTagName("troncon");
             final int nbRoadElements = roadList.getLength();
             System.out.println( "nbRoad :" + nbRoadElements);
 
-            /*
+            /**
              * Reading of all segments in the file
              */
             for (int segmentIndex = 0; segmentIndex < nbRoadElements; segmentIndex++) {
