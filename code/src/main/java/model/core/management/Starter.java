2169c13c19d8ad75e76a096614fd52eb7ef0b289
cmc373 <milinaire.c@gmail.com>
2019-11-28 11:34:53 +0100

    new class for main method

---
+++ code/src/main/java/model/core/management/Starter.java
@@ -0,0 +1,17 @@
+package model.core.management;
+
+import view.MainApp;
+/**
+ * Class responsible for starting the application.
+ */
+public class Starter {
+
+    /**
+     * Entry point of the application. Starts Deli'Velov.
+     *
+     * @param args Command line arguments that should be empty.
+     */
+    public static void main(String[] args) {
+        MainApp.main(args);
+    }
+}
