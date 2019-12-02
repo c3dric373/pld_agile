20a3e085e351ad17bce9ddd23e989157c207c4f0
Thibaut Gillain <thibaut.gillain@insa-lyon.fr>
2019-12-02 10:20:52 +0100

    Auto stash before merge of "feature/cedric_martin_update_structure" and "origin/feature/cedric_martin_update_structure"

--- code/src/main/java/model/core/management/ApplicationManager.java
+++ code/src/main/java/model/core/management/ApplicationManager.java
@@ -23,10 +23,9 @@ public interface ApplicationManager {
     void loadMap(final File file);
 
     void setObserver(final UserInterface userInterface);
- is
-    a
-         fads'fj'
-    this
+
+
+    
     /**
      * Loads a tour and creates all necessary components for the project.
      *
