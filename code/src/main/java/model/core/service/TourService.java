9b241f4059c740a3dd5bccdfe76b750a466c1bdc
cmc373 <milinaire.c@gmail.com>
2019-12-03 17:12:30 +0100

    organized method

--- code/src/main/java/model/core/service/TourService.java
+++ code/src/main/java/model/core/service/TourService.java
@@ -8,8 +8,6 @@ import java.util.OptionalInt;
 public class TourService {
 
 
-  
-
     public Tour changeDeliveryOrder(final Tour tour,
                                     final List<ActionPoint> actionPoints) {
         //TODO
@@ -28,9 +26,7 @@ public class TourService {
     public Tour changePointPosition(final Graph graph, final Tour tour,
                                     final ActionPoint oldPoint,
                                     final Point newPoint) {
-        // If point not in delivery processes throw exception 
-        // else get index of point in list of delivery processes and update the
-        // new delivery process
+        // If point not in delivery processes throw exception
         List<DeliveryProcess> oldDeliveryProcesses = tour.
                 getDeliveryProcesses();
         final OptionalInt optionalOldIndexDP = DeliveryProcessService.
@@ -39,27 +35,46 @@ public class TourService {
             throw new IllegalArgumentException("oldPoint not in "
                     + "delivery Processes");
         }
-        int oldIndexDP = optionalOldIndexDP.getAsInt();
-        final DeliveryProcess oldDeliveryProcess = oldDeliveryProcesses.
-                get(oldIndexDP);
-        final ActionPoint newActionPoint = new ActionPoint(oldPoint.getTime(),
-                newPoint,oldPoint.getActionType());
-        final DeliveryProcess newDeliveryProcess = DeliveryProcessService.
-                replacePoint(oldDeliveryProcess,newActionPoint);
-        // Replacing
-        oldDeliveryProcesses.set(oldIndexDP, newDeliveryProcess);
-        //
 
+        // Finding the journeys from and to the old point
+        final boolean IS_ENDPOINT = true;
+        final List<Journey> oldJourneys = tour.getJourneys();
+        final OptionalInt optOldPredecessorJ = JourneyService.
+                findIndexPointInJourneys(oldJourneys, oldPoint.getLocation(),
+                        IS_ENDPOINT);
+        final OptionalInt optOldSuccessorJ = JourneyService.
+                findIndexPointInJourneys(oldJourneys, oldPoint.getLocation(),
+                        !IS_ENDPOINT);
+        if (optOldPredecessorJ.isEmpty()) {
+            throw new IllegalArgumentException("Point isn't endPoint of any "
+                    + "Journey");
+        }
+        if (optOldSuccessorJ.isEmpty()) {
+            throw new IllegalArgumentException("Point isn't startPoint of any "
+                    + "Journey");
+        }
         // Find the actionPoints before and after the point to be moved
+
         final List<ActionPoint> actionPoints = tour.getActionPoints();
         final int oldPointIndex = actionPoints.indexOf(oldPoint);
-        if (oldPointIndex == -1){
+        if (oldPointIndex == -1) {
             throw new IllegalArgumentException("oldPoint not in list");
         }
         final ActionPoint predecessorPoint = actionPoints.
                 get(oldPointIndex - 1);
         final ActionPoint successorPoint = actionPoints.
                 get(oldPointIndex - 1);
+        // Create a new actionPoint and replace the old one in the list of
+        // delivery processes
+
+        int oldIndexDP = optionalOldIndexDP.getAsInt();
+        final DeliveryProcess oldDeliveryProcess = oldDeliveryProcesses.
+                get(oldIndexDP);
+        final ActionPoint newActionPoint = new ActionPoint(oldPoint.getTime(),
+                newPoint, oldPoint.getActionType());
+        final DeliveryProcess newDeliveryProcess = DeliveryProcessService.
+                replacePoint(oldDeliveryProcess, newActionPoint);
+        oldDeliveryProcesses.set(oldIndexDP, newDeliveryProcess);
 
         // Calculate shortest path between predecessor and new Point and between
         // the successor and the new Point
@@ -68,30 +83,14 @@ public class TourService {
                 predecessorPoint.getLocation(), newPoint);
         final Journey newSuccessorJourney = GraphService.shortestPath(graph,
                 predecessorPoint.getLocation(), newPoint);
+        //Replacing the old Journeys with the newly calculated ones
 
-        // Finding the journeys from and to the old point replacing them with
-        // the newly calculated journeys.
-        final boolean IS_ENDPOINT = true;
-        final List<Journey> oldJourneys = tour.getJourneys();
-        final OptionalInt optOldPredecessorJ = JourneyService.
-                findIndexPointInJourneys(oldJourneys,oldPoint.getLocation(),
-                        IS_ENDPOINT);
-        final OptionalInt optOldSuccessorJ = JourneyService.
-                findIndexPointInJourneys(oldJourneys,oldPoint.getLocation(),
-                        !IS_ENDPOINT);
-        if(optOldPredecessorJ.isEmpty()){
-            throw new IllegalArgumentException("Point isn't endPoint of any " 
-                    + "Journey");
-        }
-        if(optOldSuccessorJ.isEmpty()){
-            throw new IllegalArgumentException("Point isn't startPoint of any "
-                    + "Journey");
-        }
         tour.getJourneys().set(optOldPredecessorJ.getAsInt(),
                 newPredecessorJourney);
-        tour.getJourneys().set(optOldSuccessorJ.getAsInt(),newSuccessorJourney);
+        tour.getJourneys().set(optOldSuccessorJ.getAsInt(),
+                newSuccessorJourney);
         return tour;
-}
+    }
 
 
 }
