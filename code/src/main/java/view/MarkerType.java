e31e992d92d473d6f398db39a8f3ca068784d621
cmc373 <milinaire.c@gmail.com>
2019-12-13 20:06:19 +0100

    add marker typ

--- code/src/main/java/view/MarkerType.java
+++ code/src/main/java/view/MarkerType.java
@@ -1,4 +1,21 @@
 package view;
 
-public class MarkerType {
+import lombok.Getter;
+
+//Enum Marker Types.
+@Getter
+enum MarkerType {
+    PICKUP("Pick-Up Point", "P", "icons/marker.png"), DELIVERY("Delivery "
+            + "Point", "D", "flag.png"), BASE("Base Point", "B", "home" +
+            "-icon" + "-silhouette.png");
+
+    private String title = "";
+    public String firstLetter = "";
+    private String iconPath = "";
+
+    MarkerType(String title, String firstLetter, String iconPath) {
+        this.title = title;
+        this.firstLetter = firstLetter;
+        this.iconPath = iconPath;
+    }
 }
