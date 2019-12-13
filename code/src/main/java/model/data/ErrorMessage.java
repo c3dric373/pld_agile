c354b26271ee5cfce65e952d326f08c40a20d3da
Corentin <colapala@gmail.com>
2019-12-13 14:12:28 +0100

    checkstyle ErrorMessage

--- code/src/main/java/model/data/ErrorMessage.java
+++ code/src/main/java/model/data/ErrorMessage.java
@@ -6,119 +6,143 @@ import lombok.Getter;
  */
 public enum ErrorMessage implements GenData {
 
+    /**
+     * Path is null.
+     */
     PATH_NULL("Path should not be null"),
 
+    /**
+     * Path is empty.
+     */
     PATH_EMPTY("Path should not be empty"),
 
+    /**
+     * error during the Xml load.
+     */
     XML_LOAD_ERROR("Error during the XML load"),
 
+    /**
+     * Point doesn't exist.
+     */
     POINT_DOESNT_EXIST("Point doesn't exist"),
 
+    /**
+     * ActionType is null.
+     */
     ACTION_TYPE_NULL("Action Type should not be null"),
 
+    /**
+     * Time is null.
+     */
     TIME_NULL("Time should not be null"),
 
+    /**
+     * ActionPoint is null.
+     */
     ACTION_POINT_NULL("Action Point should not be null"),
 
+    /**
+     * DeliveryPoint is null.
+     */
     DELIVERY_POINT_NULL("Delivery point should not be null"),
 
+    /**
+     * Pickup^Point is null.
+     */
     PICKUP_POINT_NULL("Pickup point should not be null"),
 
+    /**
+     * list of ActionPoint is null.
+     */
     ACTION_POINT_LIST_NULL("Action Point list should not be null"),
 
+    /**
+     * list of ActionPoint contains null Elements.
+     */
     ACTION_POINT_LIST_NULL_ELEMENT("Action Point list's Elements should not be null"),
 
+    /**
+     * list of ActionPoint is empty.
+     */
     ACTION_POINT_LIST_EMPTY("Action Point list should not be empty"),
 
-    PICKUP_IS_DELIVERY("Pickup Point should not be Delivery Point"),
-
+    /**
+     * Point is null.
+     */
     POINT_NULL("Point should not be null"),
 
-    POINT_NOT_IN_LIST("Point is not in the list"),
-
+    /**
+     * List of Point is null.
+     */
     POINT_LIST_NULL("List of points should not be null"),
 
-    POINT_LIST_NULL_ELEMENT("List of points should not contain null Elements"),
+    /**
+     * List of Point contains null elements.
+     */
+    POINT_LIST_NULL_ELEMENT("List of points should not contain "
+            + "null Elements"),
 
-    POINT_LIST_NEED_2_ELEMENT("List of points should contain at least 2 points"),
-
-    NEGATIVE_LENGTH("Length should be positive"),
-
-    LENGTH_ZERO("Length should not be equal to zero"),
-
-    LATITUDE_IS_NULL("Latitude is null"),
-
-    LATITUDE_TO_SMALL("Latitude is too small"),
-
-    LATITUDE_TO_GREAT("Latitude is too great"),
-
-    LONGITUDE_IS_NULL("Longitude is null"),
-
-    LONGITUDE_TO_SMALL("Longitude is too small"),
-
-    LONGITUDE_TO_GREAT("Longitude is too great"),
-
-    NEGATIVE_ID("Id sould not be negative"),
-
+    /**
+     * Application is not oppened.
+     */
     APPLICATION_NOT_OPENED("Application not opened"),
 
+    /**
+     * Map is not loaded.
+     */
     MAP_NOT_LOADED("Map not loaded"),
 
+    /**
+     * File is null.
+     */
     FILE_NULL("File should not be null"),
 
+    /**
+     * Tour is not loaded.
+     */
     TOUR_NOT_LOADED("Tour should be loaded"),
 
+    /**
+     * Tour is not calculated.
+     */
     TOUR_NOT_CALCULATED("Tour should be calculated"),
 
+    /**
+     * Tour is null.
+     */
     TOUR_NULL("Tour should not be null"),
 
+    /**
+     * DeliveryProcess is null.
+     */
     DELIVERY_PROCESS_NULL("DeliveryProcess is null"),
 
+    /**
+     * Point doesn't belong to the map.
+     */
     POINT_NOT_ON_MAP("Point does not belong to the map"),
 
-    ACTION_POINT_NOT_IN_DELIVERY(" Action Point do not belong to any deliveryProcess"),
+    /**
+     * Impossible to remove delivery.
+     */
+    IMPOSSIBLE_TO_REMOVE_DELIVERY("IT is not possible to delete "
+            + "the delivery because the delivery Process does not exist"),
 
-    NAME_NULL("Name is null"),
+    /**
+     * Another action is in progress.
+     */
+    ANOTHER_ACTION_IN_PROGRESS("There is another action in progress, the "
+            + "changes can't be made yet."),
 
-    GRAPH_NULL("Graph should not be null"),
+    /**
+     * File corrupted.
+     */
+    FILE_CORRUPTED("The file you chose hasn't the right datas. Maybe you "
+            + "should check the file"),
 
-    ID_NOT_IN_GRAPH("Id do not belong to the graph"),
-
-    ID_NULL("Id should not be null"),
-
-    JOURNEY_NULL("Journey should not be null"),
-
-    JOURNEY_LIST_NULL("Journeys'list should not be null"),
-
-    JOURNEY_LIST_EMPTY("Journeys'list should not be empty"),
-
-    JOURNEY_LIST_NULL_ELEMENT("Journeys'list should not contain null elements"),
-
-    POINT_NOT_ACTION_POINT("Point is not an Action Point"),
-
-    LIST_NOT_SAME_SIZE("List should be of the same size"),
-
-    NO_ENDPOINT("Point is not an Endpoint of the journey"),
-
-    NO_STARTPOINT("Point is not a StartPoint of the journey"),
-
-    IMPOSSIBLE_TO_REMOVE_DELIVERY("IT is not possible to delete the delivery " +
-            "because the delivery Process does not exist"),
-
-    ANOTHER_ACTION_IN_PROGRESS("There is another action in progress, the " +
-            "changes can't be made yet."),
-
-    FILE_CORRUPTED("The file you chose hasn't the right datas. Maybe you " +
-            "should check the file"),
-
-    IMPOSSIBLE_TOUR("Impossible to get from the start point to the arrival point"),
-
-    DIJKSTRA_RES_NULL("Dijkstra result can't be null"),
-
-    TSP_RES_NULL("Dijkstra result can't be null"),
-
-    GRAPH_TOUR_INCOMPATIBLE("Graph and Tour are incompatible"),
-
+    /**
+     * Cannot delete the Base.
+     */
     CANNOT_DELETE_BASE("Cannot delete Base Point");
 
     /**
