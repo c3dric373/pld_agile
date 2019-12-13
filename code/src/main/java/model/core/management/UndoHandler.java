b5884835d220a74067512b0453ad935b9cea8cc3
cmc373 <milinaire.c@gmail.com>
2019-12-13 20:15:03 +0100

    management class

--- code/src/main/java/model/core/management/UndoHandler.java
+++ code/src/main/java/model/core/management/UndoHandler.java
@@ -3,8 +3,22 @@ package model.core.management;
 import model.data.Graph;
 import model.data.Tour;
 
+/**
+ * Interface that provides a way for a {@link model.data.GenDataVisitor} to
+ * communicate with an {@link ApplicationManagerImpl}.
+ */
 public interface UndoHandler {
+    /**
+     * Handle a undo with a {@link Tour} as parameter.
+     *
+     * @param tour the tour to reload.
+     */
     void undoTour(final Tour tour);
 
+    /**
+     * Handle a undo with a {@link Graph} as parameter.
+     *
+     * @param graph the tour to reload.
+     */
     void undoGraph(final Graph graph);
 }
--- code/src/main/java/model/core/management/UndoVisitor.java
+++ code/src/main/java/model/core/management/UndoVisitor.java
@@ -2,48 +2,100 @@ package model.core.management;
 
 import model.data.*;
 
+/**
+ * Visits all possible {@link GenData} that have the possiblility to generate
+ * an undo.
+ */
 public class UndoVisitor implements GenDataVisitor {
+
+    /**
+     * The model to which it should pass on the information.
+     */
     private final UndoHandler model;
 
-    UndoVisitor(UndoHandler applicationManager) {
+    /**
+     * Constructor.
+     *
+     * @param applicationManager the model to communicate with.
+     */
+    UndoVisitor(final UndoHandler applicationManager) {
         this.model = applicationManager;
     }
 
     @Override
+    /**
+     * Calls the right methods to process a {@link Tour}.
+     *
+     * @param listJourneyFromDeliveryProcess the  {@code tour} to process
+     */
     public void visit(final Tour tour) {
         this.model.undoTour(tour);
     }
 
     @Override
+    /**
+     * Calls the right methods to process a {@link Graph}.
+     *
+     * @param graph the  {@code graph} to process
+     */
     public void visit(final Graph graph) {
         this.model.undoGraph(graph);
     }
 
     @Override
-    public void visit(Point point) {
+    /**
+     * Calls the right methods to process a {@link Point}.
+     *
+     * @param point the  {@code point} to process
+     */
+    public void visit(final Point point) {
         throw new UnsupportedOperationException();
     }
 
     @Override
+    /**
+     * Calls the right methods to process a {@link DeliveryProcess}.
+     *
+     * @param deliveryProcess the  {@code deliveryProcess} to process
+     */
     public void visit(DeliveryProcess deliveryProcess) {
         throw new UnsupportedOperationException();
 
     }
 
     @Override
-    public void visit(ActionPoint actionPoint) {
+    /**
+     * Calls the right methods to process a {@link ActionPoint}.
+     *
+     * @param actionPoint the  {@code actionPoint} to process
+     */
+    public void visit(final ActionPoint actionPoint) {
         throw new UnsupportedOperationException();
 
     }
 
     @Override
-    public void visit(ListJourneyFromDeliveryProcess listJourneyFromDeliveryProcess) {
+    /**
+     * Calls the right methods to process a
+     * {@link ListJourneyFromDeliveryProcess}.
+     *
+     * @param listJourneyFromDeliveryProcess the  {@code
+     * ListJourneyFromDeliveryProcess} to process
+     */
+    public void visit(final ListJourneyFromDeliveryProcess ListJourneyFromDeliveryProcess) {
         throw new UnsupportedOperationException();
 
     }
 
     @Override
-    public void visit(ErrorMessage error) {
+    /**
+     * Calls the right methods to process a
+     * {@link ErrorMessage}.
+     *
+     * @param error the  {@code
+     * error} to process
+     */
+    public void visit(final ErrorMessage error) {
         throw new UnsupportedOperationException();
     }
 }
